4cfc44f074abec16421c396187857e6d8e8f4691
hritik01478 <hritikkumar01478@gmail.com>
2022-06-17 02:05:20 +0530

    sidebar styling done

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -3,22 +3,25 @@ const Menu = ({ openMenu, setOpenMenu }) => {
     const handleClick = () => {
         setOpenMenu(!openMenu);
         const element = document.getElementById("menu");
-        if (openMenu === true) {
-            element.classList.add("active");
-        }
-        else {
+        if (openMenu === false) {
             element.classList.remove("active");
         }
+        // else {
+        //     element.classList.add("active");
+        // }
     }
     return (
 
         < div className='m' id='menu' >
-            <ul>
-                <li onClick={handleClick}><a href="#intro">Home</a></li>
-                <li onClick={handleClick} > <a href="#about">About Me</a></li>
-                <li onClick={handleClick}><a href="#projects">Projects</a></li>
-                <li onClick={handleClick}><a href="#contact">Contact</a></li>
-            </ul>
+            <div className="m-top"></div>
+            <div className="m-data">
+                <ul>
+                    <li onClick={handleClick}><a href="#intro">Home</a></li>
+                    <li onClick={handleClick} > <a href="#about">About Me</a></li>
+                    <li onClick={handleClick}><a href="#projects">Projects</a></li>
+                    <li onClick={handleClick}><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
         </div >
     );
 }
